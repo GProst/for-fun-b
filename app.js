18aae1178bc7d8f55e52f56be06d5bd4026ef780
GProst <gprostmail@gmail.com>
2017-06-10 14:26:26 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const express = require('express')
 const app = express()
 const compression = require('compression')
@@ -7,9 +9,9 @@ app.use(compression({level: 9}))
 app.use('/', express.static(path.join(__dirname, 'assets')))
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'))
+  res.sendFile(path.join(__dirname, 'public/index.html'))
 })
 
 app.listen(3000, () => {
-    console.log('Server listening on port 3000!')
+  console.log('Server listening on port 3000!')
 })
