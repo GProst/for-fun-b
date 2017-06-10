8c40fd9d89ea56c69d0c2ec306662af32f37b44b
GProst <gprostmail@gmail.com>
2017-06-10 19:04:48 +0300

    Add MongoDB to the project

--- app.js
+++ app.js
@@ -7,6 +7,30 @@ const path = require('path')
 const morgan = require('morgan')
 const winston = require('winston')
 
+const MongoClient = require('mongodb').MongoClient
+
+// Connection URL
+const dbUrl = 'mongodb://localhost:27017/myTestDatabase'
+
+// Use connect method to connect to the server
+MongoClient.connect(dbUrl, function(err, db) {
+  if (err) {
+    winston.error('error on connecting db', err)
+  }
+  winston.info('Connected successfully to MongoDB server')
+
+  // Get the documents collection
+  const collection = db.collection('myCollection')
+  // Find some documents
+  collection.find({}).toArray((err, docs) => {
+    winston.info('Found the following records')
+    winston.info(docs)
+
+    db.close()
+  })
+})
+
+
 app.use(morgan('combined'))
 app.use(compression({level: 9}))
 app.use('/', express.static(path.join(__dirname, 'assets')))
